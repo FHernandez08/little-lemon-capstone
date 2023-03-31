1c7090ea37561e43ee246091c59e558635168187
Fidel A Hernandez Jr <fidelhernandez_8@hotmail.com>
2023-03-31 11:55:50 -0500

    commit with updated data

--- little-lemon-capstone/src/App.js
+++ little-lemon-capstone/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./layouts/NavBar";
+import Footer from "./layouts/Footer";
+import Home from "./pages/Home";
+import Booking from "./pages/Booking";
+import BookingConfirmation from "./pages/BookingConfirmation";
+import { useFormContext } from "./store/FormContext";
+import "./App.css";
 
 function App() {
+  const { form } = useFormContext();
+
+  const formProps = {
+    name: form.name,
+    date: form.date,
+    time: form.time,
+    guests: form.numberOfGuests,
+    occasion: form.occasion,
+    table: form.tablePreference,
+    request: form.message,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route
+          path="/booking-confirmation"
+          element={<BookingConfirmation {...formProps} />}
+        />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
